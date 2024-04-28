0aacdaa4d0b8d58785292ec71e4517fb1ee667d3
hhh <hhh@-1>
2024-04-28 18:04:21 +0300

    Добавьте файлы проекта.

---
+++ lab11_bebin/lab11_bebin.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    int a, b;
+    int* ptrA, * ptrB;
+
+    cout << "значення а: ";
+    cin >> a;
+    cout << "значення b: ";
+    cin >> b;
+
+    ptrA = &a;
+    ptrB = &b;
+
+    *ptrA *= 2;
+
+    int temp = *ptrA;
+    *ptrA = *ptrB;
+    *ptrB = temp;
+
+    cout << "нове значення a: " << a << endl;
+    cout << "нове значення b: " << b << endl;
+
+    return 0;
+}
